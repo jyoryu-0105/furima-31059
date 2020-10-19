c60e8c6d40bb2a5c659ebc585f7e30188ee83e9b
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-19 20:49:22 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates :nickname,        presence: true
+    validates :birthday,        presence: true
+  with_options presence: true, format: { with: /\A[一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name,      presence: true
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name,       presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana,  presence: true
+  end
 end
