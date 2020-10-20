23b52a700e9468115c2cdf1bd9dccdb67c15aa66
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-20 18:09:27 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,22 @@
 class ItemsController < ApplicationController
+  
   def index
   end
+
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      redirect_to new_item_path(@item)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :details).merge(user_id: current_user.id)
   end
 end
