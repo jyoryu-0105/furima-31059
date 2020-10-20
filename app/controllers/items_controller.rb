84df34775bcf7e14f993ba8555220637463d7eea
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-20 18:50:24 +0900

    active_hashの導入とデータの生成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  
+
   def index
   end
 
@@ -13,10 +13,11 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       redirect_to new_item_path(@item)
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :price, :details).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :category_id, :price, :status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :details).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :shipping_cost, :shipping_date, :prefecture
+  
   belongs_to :user
   has_one_attached :image
 end
