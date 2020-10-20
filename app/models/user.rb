79fc77532761236e5a2bb03cf7ae360a2c54d823
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-20 15:16:44 +0900

    バリデーションのメッセージの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
 
     validates :nickname,        presence: true
     validates :birthday,        presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字を使用してください' } do
     validates :first_name,      presence: true
     validates :last_name,       presence: true
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true,  format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナを使用してください' } do
     validates :first_name_kana, presence: true
     validates :last_name_kana,  presence: true
   end
+    validates :password,        format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:'は半角英数字を混合して使用してください'}
 end
