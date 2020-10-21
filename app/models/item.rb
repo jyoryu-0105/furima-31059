fde4cae1ff92c71690f8da47e528df4384e27b9e
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-21 16:29:24 +0900

    アソシエーションの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :shipping_cost, :shipping_date, :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_date
+  belongs_to_active_hash :prefecture
 
   belongs_to :user
   has_one_attached :image
