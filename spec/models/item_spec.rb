cd74f1e92b98443f17e46a1fe01c59aa969a3d53
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-21 18:58:26 +0900

    テストコードの修正（各IDが空の場合のテストコードの追加）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,10 +34,22 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Category must be other than 1')
     end
 
+    it 'カテゴリーの情報が必須であること(空の場合)' do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+    end
+
     it '商品の状態についての情報が必須であること' do
       @item.status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Status must be other than 1')
+    end
+
+    it '商品の状態についての情報が必須であること(空の場合)' do
+      @item.status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
     end
 
     it '配送料の負担についての情報が必須であること' do
@@ -46,16 +58,34 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Shipping cost must be other than 1')
     end
 
+    it '配送料の負担についての情報が必須であること(空の場合)' do
+      @item.shipping_cost_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping cost can't be blank", "Shipping cost is not a number")
+    end
+
     it '発送元の地域についての情報が必須であること' do
       @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
     end
 
+    it '発送元の地域についての情報が必須であること(空の場合)' do
+      @item.prefecture_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+    end
+
     it '発送までの日数についての情報が必須であること' do
       @item.shipping_date_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Shipping date must be other than 1')
+    end
+
+    it '発送までの日数についての情報が必須であること(空の場合)' do
+      @item.shipping_date_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping date can't be blank", "Shipping date is not a number")
     end
 
     it '価格についての情報が必須であること' do
