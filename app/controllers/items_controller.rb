15a6a5983c7a0c5cf8d8e4ae630ae0f376b3a6ba
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-22 18:14:20 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,8 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
     if item.user_id == current_user.id
-    item.destroy
-    redirect_to root_path
+      item.destroy
+      redirect_to root_path
     end
   end
 
