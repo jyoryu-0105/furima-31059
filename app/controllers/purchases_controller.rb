198dfc96e9cccf21be9de69f661c8c28e1eeedfd
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-26 17:41:56 +0900

    ビューファイルの調整

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,11 @@
 class PurchasesController < ApplicationController
+  before_action :set_item
+  
   def index
+  end
+  
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
