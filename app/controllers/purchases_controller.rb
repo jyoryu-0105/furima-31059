b94eef247486f0e4560f50199f1e3068c974335a
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-26 18:18:16 +0900

    購入アクションの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,9 +2,24 @@ class PurchasesController < ApplicationController
   before_action :set_item
   
   def index
+    @purchase = UserPurchase.new
   end
   
+  def create
+    @purchase = UserPurchase.new(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
   private
+  def purchase_params
+    params.require(:user_purchase).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
   def set_item
     @item = Item.find(params[:item_id])
   end
--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,12 +1,18 @@
 class UserPurchase
+  
   include ActiveModel::ActiveModel
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number
-
-  validates :city, :house_number, presence: true
+  attr_accessor :item_id, :user_id,:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number
 
   with_options presence: true do
+    validates :city
+    validates :house_number
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: { with: /0\d{10}/, message: "is invalid. NO hyphen(-)"}
+  end
+
+  def save
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
