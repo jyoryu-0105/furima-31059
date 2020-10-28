7c715b94e22f1f1931db8a21e9eebc5576f39045
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-28 20:54:39 +0900

    商品出品完了ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,9 +28,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
+    unless @item.save
       render :new
     end
   end
