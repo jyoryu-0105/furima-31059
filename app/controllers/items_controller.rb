1a9f56fc122735a72764ab7f7d850c3c4748a821
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-29 15:51:38 +0900

    商品削除ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     if item.user_id == current_user.id
       item.destroy
-      redirect_to root_path
     end
   end
 
