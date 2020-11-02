c37d66f652693549455ce24a30af4060ecb27268
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-11-02 18:55:07 +0900

    マイページ雛形の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 require("../calc")
 require("../card")
 require("../links")
-require("../select")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
