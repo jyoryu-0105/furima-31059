9e20ce4a9227a4478c2bc325c2969b3c0505814e
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-05-11 18:19:38 +0900

    unicornの設定変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.181.49.226', user: 'ec2-user', roles: %w{app db web}
+server '13.114.249.90', user: 'ec2-user', roles: %w{app db web}
